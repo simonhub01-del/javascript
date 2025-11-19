8b43306e3a57a88f281ff910123e871fa521e7d0
simon-hub11 <simonhub01@gmail.com>
2025-11-19 15:53:40 +0545

    updated

---
+++ index1.js
@@ -0,0 +1,13 @@
+function number(a, b, c) {
+  if (a > b && a > c) {
+    console.log("Number a is the greatest");
+  }
+  else if (b > a && b > c) {
+    console.log("Number b is the greatest");
+  }
+  else {
+    console.log("Number c is the greatest");
+  }
+}
+
+number(6, 3, 9); 
