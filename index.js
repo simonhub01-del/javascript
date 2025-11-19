70103f7e461c726e8e6e6cb46d31c11e8381b340
simon-hub11 <simonhub01@gmail.com>
2025-11-19 15:48:39 +0545

    updated

---
+++ index.js
@@ -0,0 +1,14 @@
+function OE(a){
+    if(a%2==0){
+        console.log("the number is Even");
+
+    }
+    else {
+        console.log("the number is odd");
+
+    }
+    return a;
+}
+
+
+        
